0215ce8f9bf758c9c57111eaf9d98f401c5d6262
Elina <ardentlycurious101@gmail.com>
2019-08-01 01:44:02 -0700

    Get image recognition results back from V3 model.

--- SeeFood/ViewController.swift
+++ SeeFood/ViewController.swift
@@ -34,8 +34,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
                 fatalError("Could not convert to CIImage.")
             }
             
-            
-            
+            detect(image: ciimage)
         }
         
         imagePicker.dismiss(animated: true, completion: nil)
@@ -43,9 +42,29 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     }
 
     func detect(image: CIImage) {
+        
         guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else {
             fatalError("Loading CoreML model failed.")
         }
+        
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            
+            guard let results = request.results as? [VNClassificationObservation] else {
+                fatalError("Model failed to process image.")
+            }
+            
+            print(results)
+            
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: image)
+        
+        do {
+            try handler.perform([request])
+        } catch {
+            print(error)
+        }
+        
     }
     
     @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
